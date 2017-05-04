6db0bd94b0083b824e429ec5aae2d9f316d7b50a
VincentPera <pera@etud.insa-toulouse.fr>
2017-05-04 19:32:34 +0200

    Audio OK
    Audio OK

--- src/view/Audio.java
+++ src/view/Audio.java
@@ -1,16 +1,9 @@
 package view;
 
-import javax.sound.sampled.AudioFileFormat;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.UnsupportedAudioFileException;
-import javax.sound.sampled.spi.AudioFileReader;
-import java.applet.Applet;
-import java.applet.AudioClip;
+import javafx.embed.swing.JFXPanel;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
 
 public class Audio extends Thread{
 
@@ -20,8 +13,7 @@ public enum typeAudio {
     RECEP
 }
 
-    private URL u1;
-    private AudioClip s1;
+    MediaPlayer m;
 
     public Audio (typeAudio t){
         if (t == typeAudio.CO){
@@ -34,38 +26,28 @@ public enum typeAudio {
     }
 
     private void connectAud() {
-        try {
-            AudioInputStream A = AudioSystem.getAudioInputStream(new File("./ressources/connect.wav"));
-        } catch (UnsupportedAudioFileException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+            String bip = "src/resources/connect.wav";
+            Media hit = new Media(new File(bip).toURI().toString());
+            JFXPanel j = new JFXPanel() ;
+            m = new MediaPlayer(hit);
     }
 
     private void deconnectAud() {
-        try {
-            AudioInputStream A = AudioSystem.getAudioInputStream(new File("./ressources/deconnect.wav"));
-        } catch (UnsupportedAudioFileException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        String bip = "src/resources/deconnect.wav";
+        Media hit = new Media(new File(bip).toURI().toString());
+        JFXPanel j = new JFXPanel() ;
+        m = new MediaPlayer(hit);
     }
 
     private void notifAud() {
-        try {
-
-            AudioInputStream A = AudioSystem.getAudioInputStream(new File("./ressources/notif.wav"));
-        } catch (UnsupportedAudioFileException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        String bip = "src/resources/notif.wav";
+        Media hit = new Media(new File(bip).toURI().toString());
+        JFXPanel j = new JFXPanel();
+        m = new MediaPlayer(hit);
     }
 
     public void play() {
-        s1.play();
+        m.play();
     }
 
     public void run(){
